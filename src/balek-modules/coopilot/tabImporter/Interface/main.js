90601c58be70bd77669cd500b087cd83555146c5
Blake Glanville <blake@digivigil.com>
2021-12-07 11:01:20 -0800

    Importer update header footer and autotrim
    Should be usable to get a column comma list from tabbed or comma separated files

--- src/balek-modules/coopilot/tabImporter/Interface/main.js
+++ src/balek-modules/coopilot/tabImporter/Interface/main.js
@@ -55,7 +55,7 @@ define(['dojo/_base/declare',
 
             importCompleteCallback: null,
 
-            _fileInfoPane: null,
+            _autoTrimPane: null,
             _previewPane: null,
             _outputPane: null,
             _outputPreviewPane: null,
@@ -66,18 +66,28 @@ define(['dojo/_base/declare',
             lines: null,
             headerStart: 0,
             footerStart: 0,
+            autoTrim: false,
+            valueSeparator: "\t",
+
+            fileData: "",
+
+            fileSize: 0,
+            fileDateString: "",
+            fileName: "",
+
+            inputType: "",
+
 
             mostValuesInLine: 0,
 
             constructor: function (args) {
                 this.values = Array()
                 this.lines = Array()
+
                 declare.safeMixin(this, args);
                 domConstruct.place(domConstruct.toDom("<style>" + this.templateCssString + "</style>"), win.body());
 
                 this.setContainerName("📥 - Importer");
-
-
 
             },
 
@@ -131,20 +141,18 @@ define(['dojo/_base/declare',
 
                 let file = files[0];
 
+
                 if (file.type.match('text.*')) {
                     let reader = new FileReader();
                     reader.onload = lang.hitch(this, function (onLoadEvent) {
-                   console.log(onLoadEvent.target.result)
-                        this._previewPane.innerHTML = "";
+                        let fileDate = new Date(file.lastModified)
+                        this.fileDateString = fileDate.toLocaleDateString("en-US")
+                        this.fileSize = Math.round(file.size/1024) +"kb"
+                        this.fileName = file.name
 
-                        this._fileInfoPane.innerHTML = ""
-                        this._fileInfoPane.innerHTML += "Importing: " + file.name;
-                        this._fileInfoPane.innerHTML += " size:" + file.size/1024;
+                        this.inputType="file"
 
-                        this._fileInfoPane.innerHTML += " lastModified:" + Date(file.lastModified);
-
-
-
+                        this.refreshInfo()
                         this.parseTabSeperatedString( onLoadEvent.target.result)
                     });
                     // Read in the image file as a data URL.
@@ -158,6 +166,12 @@ define(['dojo/_base/declare',
 
               //  this._textFile.files = files
                 console.log("Dropped",files)
+            },
+            _autoTrimMouseDown: function(mouseEvent){
+                this.autoTrim = !this.autoTrim
+                this.headerStart = 0;
+                this.footerStart = 0;
+                this.parseTabSeperatedString(this.fileData)
             },
             _onDragEnter: function(dragEvent){
                 dragEvent.preventDefault()
@@ -173,22 +187,32 @@ define(['dojo/_base/declare',
             parseTabSeperatedString: function(stringToParse)
             {
 
+                this.fileData = stringToParse;
+
+                this.mostValuesInLine = 0;
                 this.lines = []
                 let linesArray = stringToParse.split("\n");
 
                 for (const line of linesArray)
                 {
 
-                    let valuesArray = line.split("\t");
+                    let valuesArray = line.split(this.valueSeparator);
                     let valuesToSaveArray = [];
 
                     if (this.mostValuesInLine < valuesArray.length){
                         this.mostValuesInLine = valuesArray.length
-                        this.headerStart = this.lines.length
+                        if(this.autoTrim)
+                        {
+                            this.headerStart = this.lines.length
+                        }
                     }
 
                     if (this.mostValuesInLine > valuesArray.length){
-                       this.footerStart = this.lines.length
+                        if(this.autoTrim)
+                        {
+                            this.footerStart = this.lines.length - 1
+                        }
+
                     }
 
                     for (const value of valuesArray) {
@@ -200,13 +224,92 @@ define(['dojo/_base/declare',
                     this.lines.push(valuesToSaveArray)
 
                 }
+                if(!this.autoTrim && this.footerStart == 0)
+                {
+                    this.footerStart = this.lines.length - 1
+                }
 
-                this._fileInfoPane.innerHTML += "\n Lines: "+ linesArray.length;
-                this._fileInfoPane.innerHTML += " Columns: "+ this.mostValuesInLine;
-                this._fileInfoPane.innerHTML += " Header: "+ this.headerStart;
-                this._fileInfoPane.innerHTML += " Footer: "+ this.footerStart;
+                this.refreshUI()
+            },
+            _onSeparatorChoiceDown: function() {
+                if (this.valueSeparator == "\t")
+                {
+                    this.valueSeparator = ","
+                    this._separatorChoicePane.innerHTML = "Comma"
 
+                }else
+                {
+                    this.valueSeparator = "\t"
+                    this._separatorChoicePane.innerHTML = "Tab"
+
+                }
+                this.parseTabSeperatedString(this.fileData)
+            },
+            setColumnHeaderActions: function(headerNode, valuePosition){
+                let tableClassString = this.getColumnClass(valuePosition)
+                on(headerNode, "mouseenter", function (mouseEvent){
+                    //query(String("." + tableClassString)).style("backgroundColor", "orange");
+                    let nodeList = query(String("." + tableClassString), this._previewPane);
+
+                    for ( const node of nodeList)
+                    {
+                        domStyle.set(node,"backgroundColor", "rgba(123, 178, 91, 0.8)" )
+                        domStyle.set(node,"cursor", "pointer" )
+
+                    }
+                })
+
+                on(headerNode, "mouseleave", function (mouseEvent){
+                    //query(String("." + tableClassString)).style("backgroundColor", "orange");
+                    let nodeList = query(String("." + tableClassString), this._previewPane);
+
+                    for ( const node of nodeList)
+                    {
+                        domStyle.set(node,"backgroundColor", "unset" )
+                        domStyle.set(node,"cursor", "unset" )
+
+                    }
+                })
+
+
+                let currentColumn = valuePosition.valueOf()
+                on(headerNode, "dblclick", lang.hitch(this, function (mouseEvent){
+
+                    //query(String("." + tableClassString)).style("backgroundColor", "orange");
+                    let nodeList = query(String("." + tableClassString), this._previewPane);
+
+                    let currentColumnValueArray = this.getColumnValueArray(currentColumn)
+
+                    console.log("Get currentColumnValueArray:", currentColumnValueArray);
+
+                    this._outputPreviewPane.innerHTML = currentColumnValueArray.join(",")
+                    for ( const node of nodeList)
+                    {
+                        domStyle.set(node,"backgroundColor", "unset" )
+                        domStyle.set(node,"cursor", "unset" )
+
+                    }
+                }))
+
+
+
+
+            },
+            getColumnClass: function(headerPosition){
+                return this.baseClass+"TableRowIdentityCell" + headerPosition.toString()
+            },
+            refreshUI: function(){
+                this.refreshInfo()
                 this.refreshTable()
+            },
+            refreshInfo: function(){
+                this._dataInfoPane.innerHTML="";
+                this._dataInfoPane.innerHTML += " Header: "+ this.headerStart;
+                this._dataInfoPane.innerHTML += " Footer: "+ this.footerStart;
+
+                let infoString = this.fileName + " " + this.fileSize + " [ Lines:" + this.lines.length + " | Columns: " + this.mostValuesInLine + " ]"
+
+                    this.setContainerName("📥 - Importing "+ infoString);
             },
             refreshTable: function(){
 
@@ -217,10 +320,27 @@ define(['dojo/_base/declare',
                 let table = domConstruct.create("table");
                 let tableClassString = this.baseClass+"Table"
                 domClass.add(table, tableClassString)
+                let headerRow = domConstruct.create("tr")
+                let headerCount = 0;
+                do {
+                    let colHead = domConstruct.create("th")
+                    colHead.innerHTML = headerCount;
+                    domConstruct.place(colHead, headerRow)
+
+                    this.setColumnHeaderActions(colHead, headerCount-1)
+                    headerCount++
+                }while(this.mostValuesInLine >= headerCount)
+
+                let tableHead = domConstruct.create("thead")
+
+                domConstruct.place(headerRow, tableHead)
+
+                domConstruct.place(tableHead, table)
+                let row = domConstruct.create("tr")
 
                 for (const line of this.lines)
                 {
-                    if( linePosition >= this.headerStart && linePosition < this.footerStart)
+                    if( linePosition >= this.headerStart && linePosition < this.footerStart+1)
                     {
                         let row = domConstruct.create("tr")
                         let rowClassString = this.baseClass+"TableRow"
@@ -232,85 +352,53 @@ define(['dojo/_base/declare',
                         domClass.add(row, rowClassString)
                         domClass.add(firstTableData, tableClassString)
 
-                        on(firstTableData, "mouseenter", function (mouseEvent){
+                        //Create First Column Row Cell With Import Line Number and Head/Foot Set
+
+                        let setSpan = domConstruct.create("span")
+                        domClass.add(setSpan, this.baseClass+"SetSpan")
+
+                        let setHeaderDiv = domConstruct.create("div")
+                        domConstruct.place(`<img src='balek-modules/coopilot/tabImporter/resources/images/triangleUp.svg' class='${this.baseClass}SetDivImage' alt="Set Header" />`, setHeaderDiv)
+                        domClass.add(setHeaderDiv, this.baseClass+"SetHeaderDiv")
+
+                        on(setHeaderDiv, "mousedown", lang.hitch(this, function (linePosition, mouseEvent ){
                             //query(String("." + tableClassString)).style("backgroundColor", "orange");
-                            let nodeList = query(String("." + tableClassString), this._previewPane);
+                            this.setHeaderRow(linePosition)
+                        }, linePosition.valueOf() ))
 
-                            for ( const node of nodeList)
-                            {
-                                domStyle.set(node,"backgroundColor", "green" )
+                        let lineNumberDiv = domConstruct.create("span")
+                        lineNumberDiv.innerHTML = linePosition.toString()
+                        domClass.add(lineNumberDiv, this.baseClass+"FileNumberSpan")
 
-                            }
-                        })
+                        let setFooterDiv = domConstruct.create("div")
+                        domClass.add(setFooterDiv, this.baseClass+"SetFooterDiv")
+                        domConstruct.place(`<img src='balek-modules/coopilot/tabImporter/resources/images/triangleDown.svg' class='${this.baseClass}SetDivImage' alt="Set Footer" />`, setFooterDiv)
 
-                        on(firstTableData, "mouseleave", function (mouseEvent){
+
+
+                        on(setFooterDiv, "mousedown", lang.hitch(this, function (linePosition, mouseEvent ){
                             //query(String("." + tableClassString)).style("backgroundColor", "orange");
-                            let nodeList = query(String("." + tableClassString), this._previewPane);
-                            console.log(nodeList)
-
-                            for ( const node of nodeList)
-                            {
-                                domStyle.set(node,"backgroundColor", "black" )
-
-                            }
-                        })
+                            this.setFooterRow(linePosition)
+                        }, linePosition.valueOf()  ))
 
 
-                        firstTableData.innerHTML = "⬆️" + linePosition + "⬇️️"
+                        domConstruct.place(setHeaderDiv, setSpan)
+
+                        domConstruct.place(setFooterDiv, setSpan)
+
+                        domConstruct.place(setSpan, firstTableData)
+
+                        domConstruct.place(lineNumberDiv, firstTableData)
+
                         domConstruct.place(firstTableData, row)
+
 
                         for( const value of line)
                         {
                             let tableData = domConstruct.create("td")
 
-                            let tableClassString = this.baseClass+"TableRowIdentityCell" + valuePosition.toString()
 
-                            domClass.add(tableData, tableClassString)
-
-                            on(tableData, "mouseenter", function (mouseEvent){
-                                //query(String("." + tableClassString)).style("backgroundColor", "orange");
-                                let nodeList = query(String("." + tableClassString), this._previewPane);
-
-                                for ( const node of nodeList)
-                                {
-                                    domStyle.set(node,"backgroundColor", "green" )
-                                    domStyle.set(node,"cursor", "pointer" )
-
-                                }
-                            })
-
-                            on(tableData, "mouseleave", function (mouseEvent){
-                                //query(String("." + tableClassString)).style("backgroundColor", "orange");
-                                let nodeList = query(String("." + tableClassString), this._previewPane);
-
-                                for ( const node of nodeList)
-                                {
-                                    domStyle.set(node,"backgroundColor", "unset" )
-                                    domStyle.set(node,"cursor", "unset" )
-
-                                }
-                            })
-
-                            let currentColumn = valuePosition.valueOf()
-                            on(tableData, "dblclick", lang.hitch(this, function (mouseEvent){
-
-                                //query(String("." + tableClassString)).style("backgroundColor", "orange");
-                                let nodeList = query(String("." + tableClassString), this._previewPane);
-
-                                let currentColumnValueArray = this.getColumnValueArray(currentColumn)
-
-                                console.log("Get currentColumnValueArray:", currentColumnValueArray);
-
-                                this._outputPreviewPane.innerHTML = currentColumnValueArray.join(",")
-                                for ( const node of nodeList)
-                                {
-                                    domStyle.set(node,"backgroundColor", "unset" )
-                                    domStyle.set(node,"cursor", "unset" )
-
-                                }
-                            }))
-
-
+                            domClass.add(tableData, this.getColumnClass(valuePosition))
 
 
                             tableData.innerHTML = value;
@@ -324,16 +412,34 @@ define(['dojo/_base/declare',
                     linePosition++
                 }
 
+                this._previewPane.innerHTML = ""
                 domConstruct.place(table, this._previewPane)
 
 
+            },
+            setHeaderRow: function(headerRow){
+                 if((headerRow <= this.lines.length) && (headerRow >= 0)){
+                    this.headerStart = headerRow
+                    this.refreshUI()
+                }else {
+                    console.log("Out of Bounds: Not Setting Header to", headerRow)
+                }
+            },
+            setFooterRow: function(footerRow){
+                if((footerRow <= this.lines.length) && (footerRow >= 0)){
+                    this.footerStart = footerRow
+                    this.refreshUI()
+                }else {
+                    console.log("Out Of Bounds: Not Setting Footer to", headerRow)
+
+                }
             },
             getColumnValueArray(columnIndex){
                 let returnIndex = []
                 let linePosition = 0
                 for( const line of this.lines)
                 {
-                    if( linePosition >= this.headerStart && linePosition < this.footerStart) {
+                    if( linePosition >= this.headerStart && linePosition < this.footerStart+1) {
 
                         returnIndex.push(line[columnIndex])
                     }
